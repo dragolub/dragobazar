7c51749910b3850ec935268d827668dcdbc2b725
dragolub <89588419+dragolub@users.noreply.github.com>
2023-10-22 10:30:25 +0300

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,14 +12,8 @@ provider "azurerm" {
   features {}
 }
 
-# resource "random_integer" "ri" {
-#   min = 1000
-#   max = 9999
-# }
-
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
-  #  name     = "ContactBookRG${random_integer.ri.result}"
   name     = var.resource_group_name
   location = var.resource_group_location
 }
